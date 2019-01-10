9e9bc02871e47260eb00313a56b594a8d932eae8
Davide Lasagna <lasagnadavide@gmail.com>
2019-01-10 22:49:31 +0000

    added linesearch with backtracking

--- include/search_ls.hpp
+++ include/search_ls.hpp
@@ -2,11 +2,47 @@
 
 #include "Park.hpp"
 
-#include "nrproblem.hpp"
 #include "error_norm.hpp"
+#include "nrproblem.hpp"
 #include "options.hpp"
 
 namespace PaNr {
+
+auto linesearch(GT&                  G,
+                ST&                  S,
+                DVector<X, NBORDER>& z,
+                DVector<X, NBORDER>& dz,
+                DVector<X, NBORDER>& tmp) {
+
+    // start with full newton step
+    double lambda = 1.0;
+
+    // but in the first iterations it might be possible that
+    // this results in a period that is too small, or even
+    // negative. Hence, we limit lambda such that it will
+    // only give an integration period equal to half the
+    // current one, not less.
+    if (z.tail(1) + dz.tail(1) < 0.5 * z.tail(1))
+        lambda = -0.5 * z.tail(1) / dz.tail(1);
+
+    // initial error
+    double e_norm_zero   = error_norm(G, S, z, tmp);
+    double e_norm_lambda = 0.0;
+
+    for (auto ls_iter = 1; ls_iter <= opts.ls_maxiter; ls_iter++) {
+        // calc error
+        e_norm_lambda = error_norm_lambda(G, S, lambda, z, dx, tmp);
+
+        // might exit earlier
+        if (e_norm_lambda < e_norm_zero) {
+            return std::make_tuple(lambda, e_norm_lambda);
+        }
+
+        lambda *= opts.ls_rho;
+    }
+
+    throw std::runtime_error("ls_maxiter reached")
+}
 
 /* main search function */
 template <typename GT,
@@ -33,9 +69,6 @@ void _search_ls(GT&&                 G, // nonlinear propagator
     // init  parameters
     int iter = 0;
 
-    // calc initial norm
-    auto e_norm = error_norm(G, S, z, tmp);
-
     // main looping
     while (true) {
         // update the linear problem with the current solution
@@ -49,12 +82,13 @@ void _search_ls(GT&&                 G, // nonlinear propagator
                     opts.gmres_verbose);
 
         // perform line search with backtracking
-        lambda, e_norm = linesearch(G, S, z, nrp.rhs());
+        auto [lambda, e_norm] = linesearch(G, S, z, nrp.rhs(), tmp);
 
         // add correction
         z = z + lambda * rnp.rhs();
 
         // print output
+        std::cout << iter << " " << e_norm << " " << lambda << "\n";
 
         // check tolerances
         if (iter >= opts.maxiter ||              //
